df764ebb81519d2624749483916bb6a88a1791c0
Rajveer Dangi <dangirajveer33@gmail.com>
2025-03-09 23:57:44 +0530

    Updated files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { MusicCommunityProvider } from "./context/MusicCommunityContext";
 import Onboarding from "./pages/Onboarding";
 import Feed from "./pages/Feed";
@@ -39,7 +38,7 @@ const App = () => (
       <MusicCommunityProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/Melody-Share">
           <AnimatedRoutes />
         </BrowserRouter>
       </MusicCommunityProvider>
